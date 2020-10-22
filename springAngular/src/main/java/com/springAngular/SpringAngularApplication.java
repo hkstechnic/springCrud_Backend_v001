711fd1e81386f339855f21934cb790174a081ba4
hkstechnic <hkstechnic@gmail.com>
2020-10-22 12:07:09 +0530

    first commit

---
+++ springAngular/src/main/java/com/springAngular/SpringAngularApplication.java
@@ -0,0 +1,14 @@
+package com.springAngular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularApplication.class, args);
+	}
+
+}
